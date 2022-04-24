cac8f4f10ec9325fa4a113863dd828c64ca8d581
frigobar123 <alessandro.cogollo@mail.polimi.it>
2022-04-24 14:01:00 +0200

    Started implementing wheel speed computation

--- src/computevelocity.cpp
+++ src/computevelocity.cpp
@@ -2,15 +2,18 @@
 
 #include "std_msgs/String.h"
 
+#include <iostream>
+#include <ros/console.h>
 #include <sensor_msgs/JointState.h>
 #include <geometry_msgs/TwistStamped.h>
+#include <chrono>
 
 double y = 0;
 double x = 0;
 
-std::vector<double> speeds;
-std::vector<double> OldPosition{ 0, 0, 0, 0};
-std::vector<double> NewPosition;
+ros::Time current_time;
+ros::Time old_time;
+
 
 enum WheelsPosition {fl, fr, rl, rr};
 
@@ -19,12 +22,31 @@ struct RobotParams {
 	double wheelRadius, wheelAlongX, wheelAlongY;
 };
 
+RobotParams RobParams;
+
 void speedFromEncoders(const sensor_msgs::JointState::ConstPtr& msg) {
-	float vfl, vfr, vrl, vrr;
+
+	float wheelSpeeds[3];
+	float wheelDeltaTicks[3];
+	float oldTicks[3];
+
+	ros::Time current_time = msg->header.stamp; 
+	float ticks_dt = (current_time - old_time).toSec();
+	float ticks_dtm = ticks_dt / 60;
+
 	for (int i = 0; i < 4; i++) {
-		 NewPosition.push_back(msg->position[i]);
+		
+		wheelDeltaTicks[i] = msg->position[i] - oldTicks[i];
+		wheelSpeeds[i] = (wheelDeltaTicks[i] / RobParams.encoderResolution) / ticks_dtm;
+		
+		// added for debug purposes
+		std::cout << std::setprecision(10) << wheelSpeeds[i] << std::endl;		
+		
+		oldTicks[i] = msg->position[i];
 	}
-	OldPosition = NewPosition;
+	
+	old_time = current_time;
+
 };
 
 int main(int argc, char **argv) {
@@ -32,7 +54,6 @@ int main(int argc, char **argv) {
 	ros::NodeHandle n;
 
 	// retrieve RobotParameters from up.launch
-	RobotParams RobParams;
 	ros::param::get("/gearRatio", RobParams.gearRatio);
 	ros::param::get("/wheelRadius", RobParams.wheelRadius);
 	ros::param::get("/wheelAlongX", RobParams.wheelAlongX);
@@ -42,7 +63,7 @@ int main(int argc, char **argv) {
 	ROS_INFO("gearRatio: %d, wheelRadius: %lf, wheelAlongX: %lf", RobParams.gearRatio, RobParams.wheelRadius, RobParams.wheelAlongX);
 
 	ros::Subscriber sub_wheel_states = n.subscribe("wheel_states", 1000, speedFromEncoders);
-	ros::Publisher velocity_update = n.advertise<geometry_msgs::TwistStamped>("cmd_vel", 1000);
+	ros::Publisher velocity_update = n.advertise<geometry_msgs::TwistStamped>("cmd_vel", 1000);	
 
 	ros::Rate loop_rate(100);
 
